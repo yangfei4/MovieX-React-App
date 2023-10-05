14f96c2e77b155dec3d91ff2a93a57237a421801
D-YF <daiyf1028@gmail.com>
2023-10-04 22:08:01 -0500

    initialize SearchBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
+import SearchBar from "./SearchBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        This is my React App.
+        <SearchBar />
       </header>
     </div>
   );
---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,4 @@
+import SearchBar from "./SearchBar";
+import { memo } from "react";
+
+export default memo(SearchBar);
