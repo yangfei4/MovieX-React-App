7024f7d8efe29307a14c3d7feb4788002773de63
D-YF <daiyf1028@gmail.com>
2023-10-04 20:54:29 -0500

    App initialization, clean files

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        This is my React App.
+      </header>
+    </div>
+  );
+}
+
+export default App;
