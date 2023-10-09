cfbc1d221669009ebf077c2327f0024aa47c8d0e
D-YF <daiyf1028@gmail.com>
2023-10-09 12:08:35 -0500

    constants for sorting operation

---
+++ src/components/ListPage/sortConstant.js
@@ -0,0 +1,15 @@
+const POPULARITY = "popularity";
+const RELEASE_DATE =  "release_date";
+const VOTE_AVERAGE =  "vote_average";
+const NONE =  "";
+const DESC =  "desc";
+const ASC =  "asc";
+
+export {
+    POPULARITY,
+    RELEASE_DATE,
+    VOTE_AVERAGE,
+    NONE,
+    DESC,
+    ASC
+};
