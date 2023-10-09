adb8465896f1c92ef98b5cd50225f371871ec12d
D-YF <daiyf1028@gmail.com>
2023-10-09 12:09:37 -0500

    TMDB image API initial configuration

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
 
@@ -8,21 +9,48 @@ import GalleryPage from './components/GalleryPage';
 import DetailView from './components/DetailView';
 
 const App = () => {
+
+  const [ApiImageConfig, SetApiImageConfig] = useState({
+      "base_url": "http://image.tmdb.org/t/p/",
+      "secure_base_url": "https://image.tmdb.org/t/p/",
+      "poster_sizes": [
+        "w92",
+        "w154",
+        "w185",
+        "w342",
+        "w500",
+        "w780",
+        "original"
+    ]
+});
+  
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const API_respons = await axios.get('https://api.themoviedb.org/3/configuration?api_key=43f32466ca1ce72ff63dd88e6eeebdcd');
+        SetApiImageConfig(API_respons.data.images);        
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar />
-      </header>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<ListPage />} />
-          <Route path="/gallery" element={<GalleryPage />} />
-          <Route path="/detail/:id" element={<DetailView />} />
-        </Routes>
-      </BrowserRouter>
-      <footer className="App-footer">
-      </footer>
-    </div>
+    <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <NavBar />
+          </header>
+          <Routes>
+            <Route path="/" element={<ListPage ApiImageConfig={ApiImageConfig}/>} />
+            <Route path="/gallery" element={<GalleryPage />} />
+            <Route path="/detail/:id" element={<DetailView />} />
+          </Routes>
+          <footer className="App-footer">
+          </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
